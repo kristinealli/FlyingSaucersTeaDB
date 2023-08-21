5ccff6de4e25ad0fe18df59c82efe03640edb7ce
Kristine Johnson <kristineallisonjohnson@outlook.com>
2023-08-20 22:13:41 -0400

    adjusted based on feedback

--- tea_table.sql
+++ tea_table.sql
@@ -1,105 +1,58 @@
--- TEA table
+-- Create tables
+
 CREATE TABLE TEA (
     tea_id SERIAL PRIMARY KEY,
     blend_name VARCHAR(255) NOT NULL,
-    tasting_notes TEXT,
+    tasting_notes VARCHAR(255),
     date_of_purchase DATE,
-    price VARCHAR(50),
+    price DECIMAL,
     origin VARCHAR(255),
-    caffeine BOOLEAN NOT NULL
+    caffeine BOOLEAN NOT NULL,
+    supplier_id VARCHAR(255) REFERENCES SUPPLIER(supplier_id),
+    variety_type VARCHAR(255) REFERENCES VARIETY(type),
 );
 
--- RATING table
 CREATE TABLE RATING (
     rating_id SERIAL PRIMARY KEY,
-    review_comments TEXT,
-    ranking VARCHAR(5)
+    review_comments VARCHAR(255),
+    ranking TINYINT CHECK (ranking BETWEEN 1 and 5),
+    tea_id INTEGER REFERENCES TEA(tea_id),
+    user_id INTEGER REFERENCES USER(user_id)
 );
 
--- SIPPER table
-CREATE TABLE SIPPER (
-    SIPPER_id SERIAL PRIMARY KEY,
+CREATE TABLE USER (
+    user_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
--- COLLECTION table
 CREATE TABLE COLLECTION (
     collection_id SERIAL PRIMARY KEY,
     collection_name VARCHAR(255) NOT NULL,
-    date_created DATE
+    date_created DATE,
+    created_by_user_id INTEGER REFERENCES USER(user_id)
 );
 
--- VARIETY table
 CREATE TABLE VARIETY (
     type VARCHAR(255) PRIMARY KEY,
-    brewing_temp_F VARCHAR(50),
-    brewing_temp_C VARCHAR(50),
-    brewing_time VARCHAR(50),
-    tsp_per_cup VARCHAR(50)
+    brewing_temp_F DECIMAL(5,2),
+    brewing_temp_C DECIMAL(5,2),
+    brewing_time_minutes TINYINT,
+    tsp_per_cup TINYINT
 );
 
--- SUPPLIER table
 CREATE TABLE SUPPLIER (
-    supplier_id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    supplier_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
     location VARCHAR(255),
-    logo VARCHAR(255),
+    logo_path VARCHAR(255),
     website VARCHAR(255)
 );
 
--- Relationship tables
-CREATE TABLE TEA_SUPPLIER_REL (
-    tea_id INT REFERENCES TEA(tea_id),
-    supplier_id VARCHAR(255) REFERENCES SUPPLIER(supplier_id),
-    PRIMARY KEY (tea_id, supplier_id)
-);
-
-CREATE TABLE SIPPER_SUPPLIER_REL (
-    SIPPER_id INT REFERENCES SIPPER(SIPPER_id),
-    supplier_id VARCHAR(255) REFERENCES SUPPLIER(supplier_id),
-    PRIMARY KEY (SIPPER_id, supplier_id)
-);
-
-CREATE TABLE SIPPER_COLLECTION_REL (
-    SIPPER_id INT REFERENCES SIPPER(SIPPER_id),
-    collection_id INT REFERENCES COLLECTION(collection_id),
-    PRIMARY KEY (SIPPER_id, collection_id)
-);
-
-CREATE TABLE RATING_SIPPER_REL (
-    rating_id INT REFERENCES RATING(rating_id),
-    SIPPER_id INT REFERENCES SIPPER(SIPPER_id),
-    PRIMARY KEY (rating_id, SIPPER_id)
-);
-
-CREATE TABLE TEA_RATING_REL (
-    tea_id INT REFERENCES TEA(tea_id),
-    rating_id INT REFERENCES RATING(rating_id),
-    PRIMARY KEY (tea_id, rating_id)
-);
-
-CREATE TABLE TEA_SIPPER_REL (
-    tea_id INT REFERENCES TEA(tea_id),
-    SIPPER_id INT REFERENCES SIPPER(SIPPER_id),
-    PRIMARY KEY (tea_id, SIPPER_id)
-);
-
-CREATE TABLE TEA_COLLECTION_REL (
-    tea_id INT REFERENCES TEA(tea_id),
-    collection_id INT REFERENCES COLLECTION(collection_id),
-    PRIMARY KEY (tea_id, collection_id)
-);
-
-CREATE TABLE TEA_VARIETY_REL (
-    tea_id INT REFERENCES TEA(tea_id),
-    type VARCHAR(255) REFERENCES VARIETY(type),
-    PRIMARY KEY (tea_id, type)
-);
-
-CREATE TABLE COLLECTION_SIPPER_REL (
-    collection_id INT REFERENCES COLLECTION(collection_id),
-    SIPPER_id INT REFERENCES SIPPER(SIPPER_id),
-    PRIMARY KEY (collection_id, SIPPER_id)
-);
+-- Create relationships
+ALTER TABLE TEA ADD CONSTRAINT fk_tea_supplier FOREIGN KEY (supplier_id) REFERENCES SUPPLIER(supplier_id);
+ALTER TABLE TEA ADD CONSTRAINT fk_tea_variety FOREIGN KEY (variety_type) REFERENCES VARIETY(type);
+ALTER TABLE COLLECTION ADD CONSTRAINT fk_collection_user FOREIGN KEY (user_id) REFERENCES USER(user_id);
+ALTER TABLE RATING ADD CONSTRAINT fk_rating_tea FOREIGN KEY (tea_id) REFERENCES TEA(tea_id);
+ALTER TABLE RATING ADD CONSTRAINT fk_rating_user FOREIGN KEY (user_id) REFERENCES USER(user_id);
